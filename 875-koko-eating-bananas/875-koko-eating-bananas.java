b34c1dc39137bf4d1a7ee5d2235f2a9872bb75ce
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-01-20 22:29:04 +0530

    Time: 4 ms (99.84%), Space: 52.1 MB (29.86%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,16 +1,37 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int H) {
-           int low = 1, high = 1000000000, k = 0;
-        while (low <= high) {
-            k = (low + high) / 2;
-            int h = 0;
-            for (int i = 0; i < piles.length; i ++) 
-                h += Math.ceil(1.0 * piles[i] / k);
-            if (h > H)
-                low = k + 1;
-            else
-                high = k - 1;
+     public int minEatingSpeed(int[] piles, int h) {     
+       long sum=0;
+       for(int i=0;i<piles.length;i++){
+           sum+=piles[i];
+       }
+       
+       int k= (int) (1+(sum-1)/h);
+       
+       while(true){
+           int next=isValid(piles,k,h);
+           if(next==0){
+               return k;
+           }
+           k=next;
+       }
+   }
+    
+    public int isValid(int[] piles,int k,int h){
+         int sum=0;
+         int increase=Integer.MAX_VALUE; 
+         for(int i=0;i<piles.length;i++){
+           int eh=(piles[i]-1)/k;
+           if(eh>0){
+               int nextK=1+(piles[i]-1)/eh;
+               if(increase>nextK){
+                   increase=nextK;
+               }   
+           }
+             sum+=(++eh);
+         }
+        if(sum<=h){
+            return 0;
         }
-        return low;
+        return increase;  
     }
 }
