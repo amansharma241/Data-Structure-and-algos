312b322939de1b8285adc452162160b193f3f139
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-04 16:50:39 +0530

    Time: 2 ms (48.53%), Space: 44.7 MB (7.37%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,25 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-      if (height.length <= 1) return 0;
-		// start 2 pointers
-        int left = 0; int right = height.length-1;
-        int sum = 0;
-        while (left < right) {
-            int max = 0;
-			// keep advancing left while height is less/equal to right pointer && we havent reached the right pointer
-            while (left < right && height[left] <= height[right]) {
-                max = Math.max(max, height[left]);
-                sum += max - height[left];
-                left++;
-            }
-            max = 0;
-			// reset max, and do the same while the right height is less than left height
-            while (left < right && height[right] < height[left]) {
-                max = Math.max(max, height[right]);
-                sum += max - height[right];
-                right--;
-            }
+      if(height==null) return 0;
+        int n = height.length;
+        int [] lmax = new int[height.length];
+        int [] rmax = new int[height.length];
+        int water = 0;
+        lmax[0] = height[0];
+        rmax[height.length-1] = height[height.length-1];
+        for(int i=1;i<height.length;i++){
+            lmax[i] = Math.max(lmax[i-1],height[i]);
+            rmax[n-i-1] = Math.max(rmax[n-i],height[n-i-1]);
         }
-        return sum;  
+        for(int i=0;i<n;i++){
+            water += Math.min(lmax[i],rmax[i]) - height[i];
+        }
+        return water;
     }
 }
