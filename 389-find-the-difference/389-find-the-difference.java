c4bfb82251b852731ff633029f4a5408fbd767ec
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-07 16:09:47 +0530

    Time: 1 ms (99.79%), Space: 40.3 MB (19.26%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,5 +1,11 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-      return (char) (s + t).chars().reduce(0, (c, d) -> c ^ d);  
+       int diff = 0;
+        int i = 0;
+        for( i = 0; i < s.length(); i++) {
+            diff = diff ^ s.charAt(i) ^ t.charAt(i);
+        }
+        diff = diff ^ t.charAt(i);
+        return (char) diff; 
     }
 }
