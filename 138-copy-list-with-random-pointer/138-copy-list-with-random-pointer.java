91774b83150ff5c3f0a9c7a66ff1835c3436e585
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-14 20:44:51 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (16.92%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,25 +15,46 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        HashMap<Node,Node> map = new HashMap<>();
-        Node curr = head;
-        Node dummy = new Node(-1);
-        Node nhead = dummy;
-        
-        while(curr!=null){
-            nhead.next = new Node(curr.val);
-            map.put(curr,nhead.next);
-            nhead = nhead.next;
-            curr = curr.next;
+       if(head == null){
+        return null;
+    }
+    
+    //copy linked list to following structure A -> A' -> B -> B' .... -> C -> C'
+    Node p = head;
+    while(p != null){
+        Node tmp = p.next;
+        p.next = new Node(p.val);
+        p.next.next = tmp;
+        p = tmp;
+    }
+    
+    //copy random list
+    p = head;
+    while(p != null){
+        if(p.random == null){
+            p.next.random = null;
+        }else{
+            p.next.random = p.random.next;
         }
-        nhead = dummy.next;
-        curr = head;
-        
-        while(nhead!=null){
-            nhead.random = (curr.random!=null ? map.get(curr.random):null);
-            nhead = nhead.next;
-            curr = curr.next;
+        p = p.next.next;
+    }
+    
+    //split structure A -> A' -> B -> B' => A -> B | A' -> B'
+    Node newHead = head.next;
+    Node q = newHead;
+    p = head;
+    
+    while(p!= null){
+        p.next = p.next.next;
+        if(p.next == null){
+            q.next = null;
+        }else{
+            q.next = p.next.next;
         }
-        return dummy.next;
+        p = p.next;
+        q = q.next;
+    }
+    
+    return newHead; 
     }
 }
