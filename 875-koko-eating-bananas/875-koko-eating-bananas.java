a4b654203f8be038ba453938f6bd51ad2716853d
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-16 18:21:58 +0530

    Time: 38 ms (41.91%), Space: 43.3 MB (74.90%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,37 +1,29 @@
 class Solution {
-     public int minEatingSpeed(int[] piles, int h) {     
-       long sum=0;
-       for(int i=0;i<piles.length;i++){
-           sum+=piles[i];
-       }
-       
-       int k= (int) (1+(sum-1)/h);
-       
-       while(true){
-           int next=isValid(piles,k,h);
-           if(next==0){
-               return k;
-           }
-           k=next;
-       }
-   }
-    
-    public int isValid(int[] piles,int k,int h){
-         int sum=0;
-         int increase=Integer.MAX_VALUE; 
-         for(int i=0;i<piles.length;i++){
-           int eh=(piles[i]-1)/k;
-           if(eh>0){
-               int nextK=1+(piles[i]-1)/eh;
-               if(increase>nextK){
-                   increase=nextK;
-               }   
-           }
-             sum+=(++eh);
-         }
-        if(sum<=h){
-            return 0;
+    public int minEatingSpeed(int[] piles, int h) {
+        int lo = 1;
+        int max = 0;
+        int maxspeed = 0;
+        for(int val: piles){
+            max = Math.max(max,val);
         }
-        return increase;  
+        int hi = max;
+        
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(isPossible(piles,h,mid)){
+                maxspeed = mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
+        }
+        return maxspeed;
+    }
+    public boolean isPossible(int [] piles , int h , int mid){
+        int hours = 0;
+        
+        for(int val: piles){
+            hours+= (int)Math.ceil((val*1.0)/mid);
+        }
+        return hours<=h;
     }
 }
