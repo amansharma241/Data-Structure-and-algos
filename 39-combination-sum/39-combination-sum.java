31be9c6bb466eec3ea040b9936b8f7c7982636a5
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-17 14:09:48 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (40.27%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,20 +1,22 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            tempList.remove(tempList.size() - 1);
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, candidates, target, new ArrayList<>(), 0, 0);
+        return res;
+    }
+    
+    private void backtrack(List<List<Integer>> res, int[] candidates, int target, List<Integer> potential, int currSum, int startIndex) {
+        if (currSum == target) {
+            res.add(new ArrayList<>(potential));
+            return;
+        }
+        for (int i = startIndex; i < candidates.length; i++) {
+            int num = candidates[i];
+            if (currSum + num <= target) {
+                potential.add(num);
+                backtrack(res, candidates, target, potential, currSum + num, i);
+                potential.remove(potential.size() - 1);
+            }
         }
     }
-}
 }
