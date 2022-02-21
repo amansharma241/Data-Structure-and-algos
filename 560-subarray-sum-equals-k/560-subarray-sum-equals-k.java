5a9afccdd486382bd04546ac00721b811260fe0d
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-22 00:24:28 +0530

    Time: 17 ms (97.70%), Space: 45 MB (81.74%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,19 +1,44 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-         int count = 0;
-      
-        int[] sum = new int[nums.length + 1];
-        sum[0] = 0;
-        for (int i = 1; i <= nums.length; i++)
-            sum[i] = sum[i - 1] + nums[i - 1];
-      
-        for (int start = 0; start < sum.length; start++) {
-            for (int end = start + 1; end < sum.length; end++) {
-                if (sum[end] - sum[start] == k)
-                    count++;
-            }
-        }
-      
+        
+        //wrong code....
+        // if(nums==null) return 0;
+        // int count = 0;
+        // int [] prefix = new int[nums.length];
+        // prefix[0] = nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     prefix[i] = nums[i] + prefix[i-1];
+        // }
+        // Set<Integer> set = new HashSet<>();
+        // set.add(prefix[0]);
+        // set.add(0);
+        // for(int i=1;i<prefix.length;i++){
+        //     if(set.contains(prefix[i]-k)) count++;
+        //     set.add(prefix[i]);
+        // }
+        // return count;
+        
+        // int count = 0;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // map.put(0,1);
+        // int sum=0;
+        // for(int i=0;i<nums.length;i++){
+        //     sum+=nums[i];
+        //     if(map.containsKey(sum-target)){
+        //         count+= map.get(sum-target);
+        //     }
+        //     map.put(sum,map.getOrDefault(map,0)+1);
+        // }
+        // return count;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum=0;
+        int count=0;
+        map.put(0,1);       
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-k)) count+=map.get(sum-k);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }     
         return count;
     }
 }
