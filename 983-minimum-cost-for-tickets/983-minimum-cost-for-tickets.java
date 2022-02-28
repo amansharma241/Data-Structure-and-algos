e1eb3fba9c9c9d685316f01e24e70c6f5e1cd2be
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-28 20:13:21 +0530

    Time: 1 ms (93.45%), Space: 40.2 MB (48.18%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,8 +1,27 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        if(days==null || costs==null) return 0;
-        int [] dp = new int[days.length];
-        return helper(days,costs,0,dp);
+         // length up to the last travel + 1 day is good enough (no need for 365)
+    int lastDay = days[days.length - 1]; 
+    // dp[i] means up to i-th day the minimum cost of the tickets
+    int[] dp = new int[lastDay + 1]; 
+    boolean[] isTravelDays = new boolean[lastDay + 1];
+    // mark the travel days
+    for(int day : days) isTravelDays[day] = true;
+    
+    for(int i = 1; i <= lastDay; i++) {
+        if(!isTravelDays[i]) { // no need to buy ticket if it is not a travel day
+            dp[i] = dp[i - 1];
+            continue;
+        }
+        // select which type of ticket to buy
+        dp[i] = costs[0] + dp[i - 1]; // 1-day
+        dp[i] = Math.min(costs[1] + dp[Math.max(i - 7, 0)], dp[i]); // 7-day
+        dp[i] = Math.min(costs[2] + dp[Math.max(i - 30, 0)], dp[i]); // 30-day
+    }
+    return dp[lastDay];
+        // if(days==null || costs==null) return 0;
+        // int [] dp = new int[days.length];
+        // return helper(days,costs,0,dp);
     }
     public int helper(int [] days,int [] costs,int i,int [] dp){
         if(i>=days.length) return 0;
