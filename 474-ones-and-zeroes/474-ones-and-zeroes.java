55daa7bd3a63657377d96a074f8f96ee367c1a61
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-28 23:57:12 +0530

    Time: 141 ms (31.97%), Space: 124.2 MB (11.99%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,15 +1,27 @@
 class Solution {
-    public int findMaxForm(String[] S, int M, int N) {
-         int[][] dp = new int[M+1][N+1];
-        for (String str : S) {
-            int zeros = 0, ones = 0;
-            for (char c : str.toCharArray())
-                if (c == '0') zeros++;
-                else ones++;
-            for (int i = M; i >= zeros; i--)
-                for (int j = N; j >= ones; j--)
-                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
-        }
-        return dp[M][N];
-    }
+    public int findMaxForm(String[] strs, int m, int n) {
+		return helper(0, m, n, strs, new Integer[strs.length][m + 1][n + 1]);
+	}
+
+	private int helper(int index, int m, int n, String[] strs, Integer[][][] memo) {
+		if (index == strs.length) return 0;
+		if (memo[index][m][n] != null) return memo[index][m][n];
+		
+		int zero = countZeros(strs[index]);
+		int one = strs[index].length() - zero;
+		
+		int res = helper(index + 1, m, n, strs, memo);
+		if (m >= zero && n >= one)
+			res = Math.max(res, 1 + helper(index + 1, m - zero, n - one, strs, memo));
+		
+		return memo[index][m][n] = res;
+	}
+
+	private int countZeros(String s) {
+		int res = 0;
+		for (char c : s.toCharArray()) {
+			if (c == '0') res++;
+		}
+		return res;
+	}
 }
