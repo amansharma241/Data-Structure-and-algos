fb34942029aa194af96b13d7992d2d5dd1608b36
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-28 15:42:52 +0530

    Time: 19 ms (95.64%), Space: 41.3 MB (63.30%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -1,8 +1,17 @@
 class Solution {
     public int numSquares(int n) {
         int [] dp = new int[n+1];
-      int ans = helper(n,dp);
-        return ans;
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            int min = Integer.MAX_VALUE;
+            for(int j=1;j*j<=i;j++){
+                min = Math.min(min,dp[i-j*j]);
+            }
+            dp[i] = min+1;
+        }
+      //int ans = helper(n,dp);
+        return dp[n];
     }
     public int helper(int n,int [] dp){
         if(n<0) return Integer.MAX_VALUE;
