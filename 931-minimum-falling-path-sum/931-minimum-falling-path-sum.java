80d5d67004021d8c3c712665f8030fea6a84eaa0
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-02-28 08:35:32 +0530

    Time: 1 ms (99.94%), Space: 48.4 MB (7.69%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,51 +1,25 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        if(matrix.length==0) return 0;
-         int row = matrix.length;
-         int col = matrix[0].length;
-         int minans = Integer.MAX_VALUE;
-//         int [][] dp = new int[row][col];
-//         for(int i=0;i<col;i++){ // copying the first row into dp
-//             dp[0][i] = matrix[0][i];
-//         }
+        if(matrix==null) return 0;
+        int n = matrix.length;
+        int m = matrix[0].length;
         
-//         for(int i=1;i<row;i++){
-//             for(int j=0;j<col;j++){
-//                 if(j==0) dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j+1]);
-//                 else if(j==col-1) dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-1]);
-//                 else {
-//                     dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-1]);
-//                     dp[i][j] = Math.min(dp[i][j],dp[i-1][j+1]);
-//                 }
-//                 dp[i][j]+=matrix[i][j];
-//             }
-//         }
-//         for(int k=0;k<col;k++){
-//             minans = Math.min(minans,dp[row-1][k]);
-//         }
-//         return minans;
+        int [][] dp = new int[n][m];
+        int minans = Integer.MAX_VALUE;
         
-        
-        // recursive + memoized version
-        int [][] dp = new int [row][col];
-        for(int i=0;i<matrix.length;i++){
-            minans = Math.min(minans,helper(matrix,0,i,dp));
+        for(int i=0;i<m;i++){
+            minans = Math.min(minans,helper(n-1,i,matrix,dp));
         }
         return minans;
     }
-    public static int helper(int [][] matrix,int row,int col,int [][] dp){
-        if(row>=matrix.length || row<0 || col<0 || col>=matrix[0].length) return Integer.MAX_VALUE;
-        if(row == matrix.length-1) return matrix[row][col];
+    public int helper(int row,int col,int [][] matrix,int [][] dp){
+        if(row==0 && col>=0 && col<matrix[0].length) return matrix[row][col];
+        if(col<0 || col>matrix[0].length-1) return Integer.MAX_VALUE;
         if(dp[row][col]!=0) return dp[row][col];
+        int top = helper(row-1,col,matrix,dp);
+        int topleft = helper(row-1,col-1,matrix,dp);
+        int topright = helper(row-1,col+1,matrix,dp);
         
-        int bottom = helper(matrix,row+1,col,dp);
-        int bottomright = helper(matrix,row+1,col+1,dp);
-        int bottomleft = helper(matrix,row+1,col-1,dp);
-        
-        int ans = Math.min(bottom,Math.min(bottomleft,bottomright));
-        
-        dp[row][col] = matrix[row][col] + ans;
-        
-        return dp[row][col];
+       return dp[row][col] = matrix[row][col] + Math.min(topright,Math.min(top,topleft));
     }
 }
