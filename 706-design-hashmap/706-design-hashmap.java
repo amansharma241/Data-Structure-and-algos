b8c5b32a4aa85f27afe8ec9f9c49b463db5231e1
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-02 18:58:59 +0530

    Time: 22 ms (78.51%), Space: 61.8 MB (50.71%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,26 +1,67 @@
 class MyHashMap {
 
-    private int[] _v = new int[1000001];
-
-    /** Initialize your data structure here. */
+     final ListNode[] nodes = new ListNode[10000];
+    
     public MyHashMap() {
-        for(int i = 0; i < 1000001; ++i)
-            _v[i] = -1;
+        
     }
     
-    /** value will always be non-negative. */
     public void put(int key, int value) {
-        _v[key] = value;
+        int i = getIndex(key);
+        if (nodes[i] == null) {
+            nodes[i] = new ListNode(-1, -1);
+        }
+        ListNode prev = find(nodes[i], key);
+        if (prev.next == null) {
+            prev.next = new ListNode(key, value);
+        } else {
+            prev.next.val = value;
+        }
     }
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return _v[key];
+        int i = getIndex(key);
+        if (nodes[i] == null) {
+            return -1;
+        }
+        ListNode node = find(nodes[i], key);
+        return node.next == null ? -1 : node.next.val;
     }
     
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        _v[key] = -1;
+        int i = getIndex(key);
+        if (nodes[i] == null) {
+            return;
+        }
+        ListNode prev = find(nodes[i], key);
+        if (prev.next == null) {
+            return;
+        }
+        prev.next = prev.next.next;
+    }
+    
+    int getIndex(int key) {
+        return Integer.hashCode(key) % nodes.length;
+    }
+    
+    ListNode find(ListNode bucket, int key) {
+        ListNode node = bucket;
+        ListNode prev = null;
+        while (node != null && node.key != key) {
+            prev = node;
+            node = node.next;
+        }
+        return prev;
+    }
+    
+    class ListNode {
+        int key, val;
+        ListNode next;
+
+        ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
     }
 }
 
