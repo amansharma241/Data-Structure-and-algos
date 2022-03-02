26009af5ad367bd3a278262844cd709ec8fdce18
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-02 23:41:17 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (87.67%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,46 +15,36 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-       if(head == null){
-        return null;
-    }
-    
-    //copy linked list to following structure A -> A' -> B -> B' .... -> C -> C'
-    Node p = head;
-    while(p != null){
-        Node tmp = p.next;
-        p.next = new Node(p.val);
-        p.next.next = tmp;
-        p = tmp;
-    }
-    
-    //copy random list
-    p = head;
-    while(p != null){
-        if(p.random == null){
-            p.next.random = null;
-        }else{
-            p.next.random = p.random.next;
+      if(head==null) return null;
+        Node curr = head;
+        
+        //form links
+        while(curr!=null){
+            Node temp = curr.next;
+            Node copy = new Node(curr.val);
+            curr.next = copy;
+            copy.next = temp;
+            curr = temp;
         }
-        p = p.next.next;
-    }
-    
-    //split structure A -> A' -> B -> B' => A -> B | A' -> B'
-    Node newHead = head.next;
-    Node q = newHead;
-    p = head;
-    
-    while(p!= null){
-        p.next = p.next.next;
-        if(p.next == null){
-            q.next = null;
-        }else{
-            q.next = p.next.next;
+        // assign random pointers
+         curr = head;
+        while(curr!=null){
+            if(curr.random!=null) curr.next.random = curr.random.next;
+            curr = curr.next.next;
         }
-        p = p.next;
-        q = q.next;
-    }
-    
-    return newHead; 
+        
+        // break links
+        curr = head;
+       Node fake = head.next;
+        Node ans = fake;
+        while(curr!=null && fake!=null){
+            curr.next = curr.next.next;
+            if(curr.next!=null){ // this line is necessary because it helps us in handling if we have a single node
+                fake.next = fake.next.next;
+            }
+            curr = curr.next;
+            fake = fake.next;
+        }
+        return ans;
     }
 }
