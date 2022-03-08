ca7e28dd997fc07d84b8fffe71db8f6457698036
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-08 14:19:51 +0530

    Time: 5 ms (85.66%), Space: 42.3 MB (58.35%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,21 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-         int[][] dp = new int[word1.length()+1][word2.length()+1];
-        for(int i = 0;i<=word1.length();i++) dp[i][0] = i;
-        for(int j = 0;j<=word2.length();j++) dp[0][j] = j;
-        for(int i = 0;i<word1.length();i++){
-            for(int j = 0;j<word2.length();j++){
-                if(word1.charAt(i) == word2.charAt(j)) dp[i+1][j+1] = dp[i][j];
-                else dp[i+1][j+1] = Math.min(dp[i][j],Math.min(dp[i+1][j],dp[i][j+1])) + 1;
+          int[][] dp = new int[word2.length()+1][word1.length()+1];
+    for(int i = 0; i <= word2.length(); i++) {
+        for(int j = 0; j <= word1.length(); j++) {
+            if(i == 0 && j == 0) dp[i][j] = 0; // no strings given
+            else if(i == 0 && j != 0) {
+                dp[i][j] = j; // word2 is empty
+            } else if(i != 0 && j == 0) {
+                dp[i][j] = i; // word1 is empty
+            } else if(word2.charAt(i-1) != word1.charAt(j-1)) {
+                dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
+            }else {
+                dp[i][j] = dp[i-1][j-1]; // same characters just carry over previous chars from both
             }
         }
-        // basically dp[i][j] store krega dp[i-1][j-1] ka answer..
-        return dp[word1.length()][word2.length()];
+    }
+    return dp[word2.length()][word1.length()];
         
         //return helper(s1,s2,0,0,dp); recursive solution....
-        // wrong solution....
-        // int len = s1.length()>s2.length()? s1.length() : s2.length();
-        // return len - lcs(s1,s2,0,0,dp);
+        
     } 
     public int helper(String s1,String s2,int i,int j,int [][] dp){
       if(i==s1.length()) return s2.length()-j;
@@ -31,16 +34,5 @@ class Solution {
         }
         return dp[i][j] = mini;
     }
-    // this is wrong code.....
-    public int lcs(String s1,String s2,int i , int j,int [][] dp){
-        if(i>=s1.length() || j>=s2.length()) return 0;
-        if(dp[i][j]!=0) return dp[i][j];
-        
-        if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j] = 1 + lcs(s1,s2,i+1,j+1,dp);
-        }
-        else{
-            return dp[i][j] =  Math.max(lcs(s1,s2,i+1,j,dp),lcs(s1,s2,i,j+1,dp));
-    }
-    }
+   
 }
