4e839b4ae1a7935603b4ab89ec1e1e054fe0b5d6
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-08 15:35:22 +0530

    Time: 6 ms (78.02%), Space: 45.4 MB (14.17%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,35 +1,35 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-          int[][] dp = new int[word2.length()+1][word1.length()+1];
-    for(int i = 0; i <= word2.length(); i++) {
-        for(int j = 0; j <= word1.length(); j++) {
-            if(i == 0 && j == 0) dp[i][j] = 0; // no strings given
-            else if(i == 0 && j != 0) {
-                dp[i][j] = j; // word2 is empty
-            } else if(i != 0 && j == 0) {
-                dp[i][j] = i; // word1 is empty
-            } else if(word2.charAt(i-1) != word1.charAt(j-1)) {
-                dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
-            }else {
-                dp[i][j] = dp[i-1][j-1]; // same characters just carry over previous chars from both
-            }
-        }
-    }
-    return dp[word2.length()][word1.length()];
+          int[][] dp = new int[word1.length()+1][word2.length()+1];
+    // for(int i = 0; i <= word2.length(); i++) {
+    //     for(int j = 0; j <= word1.length(); j++) {
+    //         if(i == 0 && j == 0) dp[i][j] = 0; // no strings given
+    //         else if(i == 0 && j != 0) {
+    //             dp[i][j] = j; // word2 is empty
+    //         } else if(i != 0 && j == 0) {
+    //             dp[i][j] = i; // word1 is empty
+    //         } else if(word2.charAt(i-1) != word1.charAt(j-1)) {
+    //             dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
+    //         }else {
+    //             dp[i][j] = dp[i-1][j-1]; // same characters just carry over previous chars from both
+    //         }
+    //     }
+    // }
+    // return dp[word2.length()][word1.length()];
         
-        //return helper(s1,s2,0,0,dp); recursive solution....
+        return helper(word1,word2,word1.length(),word2.length(),dp); //recursive solution....
         
     } 
     public int helper(String s1,String s2,int i,int j,int [][] dp){
-      if(i==s1.length()) return s2.length()-j;
-      if(j==s2.length()) return s1.length()-i;
+      if(i==0) return j;
+      if(j==0) return i;
        if(dp[i][j]!=0) return dp[i][j];
        int mini = Integer.MAX_VALUE;
-        if(s1.charAt(i)==s2.charAt(j)) return dp[i][j] = helper(s1,s2,i+1,j+1,dp);
+        if(s1.charAt(i-1)==s2.charAt(j-1)) return dp[i][j] = helper(s1,s2,i-1,j-1,dp);
         else{
-            int replace = 1 + helper(s1,s2,i+1,j+1,dp);
-            int insert = 1 + helper(s1,s2,i,j+1,dp);
-            int delete = 1 + helper(s1,s2,i+1,j,dp);
+            int replace = 1 + helper(s1,s2,i-1,j-1,dp);
+            int insert = 1 + helper(s1,s2,i,j-1,dp);
+            int delete = 1 + helper(s1,s2,i-1,j,dp);
             mini = Math.min(Math.min(replace,insert),delete);
         }
         return dp[i][j] = mini;
