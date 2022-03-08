9f1a2f4e9899666c2b7748d6d4f95c156df14264
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-08 18:09:17 +0530

    Time: 1 ms (99.83%), Space: 42.6 MB (85.64%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,13 +1,25 @@
 class Solution {
-    public int jump(int[] A) {
-        int jumps = 0, curEnd = 0, curFarthest = 0;
-	for (int i = 0; i < A.length - 1; i++) {
-		curFarthest = Math.max(curFarthest, i + A[i]);
-		if (i == curEnd) {
-			jumps++;
-			curEnd = curFarthest;
-		}
-	}
-	return jumps;
+    public int jump(int[] arr) {
+      if(arr.length<=1) return 0;
+        else if(arr[0]==0) return -1;
+        else{
+        int step = arr[0];
+        int maxreach = arr[0];
+        int jump = 1;
+        
+        for(int i=1;i<arr.length;i++){
+            if(i==arr.length-1) return jump;
+            
+            maxreach = Math.max(maxreach,i + arr[i]);
+            step--;
+            
+            if(step==0){
+                jump++;
+                if(i>=maxreach) return -1;
+                step = maxreach - i;
+            }
+        }
+        }
+        return -1;
     }
 }
