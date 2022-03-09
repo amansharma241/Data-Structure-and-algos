9a0da94bfa39e016b1ee7bb0ab09c250196a9acc
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-09 18:43:04 +0530

    Time: 1 ms (59.62%), Space: 42.6 MB (18.51%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -3,9 +3,19 @@ class Solution {
         int m = Grid.length;
         int n = Grid[0].length;
         int [][] dp = new int [m][n];
-       
-        int ans = helper(m-1,n-1,dp,Grid);
-            return ans;
+        //dp[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+              if(Grid[i][j]==1) dp[i][j]=0;
+              else if(i==0 && j==0) dp[i][j] = 1;
+              else if(i>0 && j>0) dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                else if(i>0) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+        // int ans = helper(m-1,n-1,dp,Grid);
+        //     return ans;
     }
     public static int helper(int row,int col,int [][] dp,int [][] Grid){
         if(row==0 && col==0 && Grid[0][0]!=1) return 1;
