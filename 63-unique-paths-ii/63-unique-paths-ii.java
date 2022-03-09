9c04a6edbf3c0bfb0c530ac9f873b0f1eec3482a
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-09 18:51:22 +0530

    Time: 1 ms (59.62%), Space: 42.3 MB (32.77%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -13,14 +13,14 @@ class Solution {
                 else dp[i][j] = dp[i][j-1];
             }
         }
-        return dp[m-1][n-1];
-        // int ans = helper(m-1,n-1,dp,Grid);
-        //     return ans;
+        //return dp[m-1][n-1];
+         int ans = helper(m-1,n-1,dp,Grid);
+         return ans;
     }
     public static int helper(int row,int col,int [][] dp,int [][] Grid){
-        if(row==0 && col==0 && Grid[0][0]!=1) return 1;
-        //if(Grid[row][col]==1) return 0;
         if(row<0 || col<0 || Grid[row][col]==1) return 0;
+        if(row==0 && col==0 ) return 1;
+        
         if(dp[row][col]!=0) return dp[row][col];
         return dp[row][col] = helper(row-1,col,dp,Grid) + helper(row,col-1,dp,Grid);
     
