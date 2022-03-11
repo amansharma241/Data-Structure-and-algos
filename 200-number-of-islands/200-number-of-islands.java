ea60225ceec1104dbb22b147dd1ca6f13a6e4fac
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-11 14:04:23 +0530

    Time: 3 ms (89.97%), Space: 58.5 MB (6.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -12,15 +12,13 @@ class Solution {
         }
         return count;
     }
-    public int helper(int i,int j,char [][] grid){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0') return 0;
-        
+    public void helper(int i,int j,char [][] grid){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0') return ;
         grid[i][j] = '0';
-        int up = helper(i-1,j,grid);
-        int down = helper(i+1,j,grid);
-        int left = helper(i,j-1,grid);
-        int right = helper(i,j+1,grid);
+         helper(i-1,j,grid);
+         helper(i+1,j,grid);
+         helper(i,j-1,grid);
+         helper(i,j+1,grid);
         
-        return up + down + right + left;
     }
 }
