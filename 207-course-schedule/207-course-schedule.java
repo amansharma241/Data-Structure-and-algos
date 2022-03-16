b886861f9f8aba276c263db51a109e1fa341034d
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-16 14:54:26 +0530

    Time: 4 ms (90.40%), Space: 47.1 MB (63.84%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,54 +1,28 @@
 class Solution {
-   public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // Create Array of lists -> adjecency matrix of graph
-        ArrayList<Integer>[] adj = new ArrayList[numCourses];
+    public boolean canFinish(int nums, int[][] pre) {
+      ArrayList<Integer> [] adj = new ArrayList[nums];
+      int [] visited = new int [nums];
         
-        // Fill all the nodes (0 to numCourses - 1) as array index holding newly created arraylists
-        for(int i=0; i<numCourses; i++) {
+        for(int i=0;i<adj.length;i++){
             adj[i] = new ArrayList<>();
         }
-        
-        // Fill the arraylists of each nodes with their outgoing edges/connected nodes
-        for(int[] pre : prerequisites) {
-            adj[pre[0]].add(pre[1]);
+        for(int [] curr : pre){
+            adj[curr[0]].add(curr[1]);
         }
-        
-        // Define an array of visited (0 -> unvisited, 1 -> visited, 2 -> completed), initially filled with 0's 
-        int[] visited = new int[numCourses];
-        
-        // Do DFS for each of the array nodes to check a cycle
-        for(int i=0; i<numCourses; i++) {
-            if( dfs(i, visited, adj)==false)
-                return false;
+        for(int i=0;i<nums;i++){
+            if(!dfshelper(i,adj,visited)) return false;
         }
-        
         return true;
     }
-    
-    public boolean dfs(int node, int[] visited, ArrayList<Integer>[] adj) {
-        // Return false if the node is visited and viewed again before completion
-        if(visited[node] == 1) {
-            return false;
-        }
+    public boolean dfshelper(int node , ArrayList<Integer>[] adj,int [] visited){
+        if(visited[node]==1) return false;
+        if(visited[node]==2) return true;
         
-        // Return true if the node is completed processing
-        if(visited[node] == 2) {
-            return true;
-        }
-         
-        // Mark the node as visited
         visited[node] = 1;
-        
-        // DFS of all the other nodes current "node" is connected to
-        for(int n : adj[node]) {
-            if(!dfs(n, visited, adj))
-                return false;
+        for(int curr : adj[node]){
+            if(!dfshelper(curr,adj,visited)) return false;
         }
-        
-        // If no more other nodes for the current "node" mark as completed and return true
-        
         visited[node] = 2;
-        
         return true;
     }
 }
