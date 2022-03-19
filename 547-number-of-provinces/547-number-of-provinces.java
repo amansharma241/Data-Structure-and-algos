b2e407d592e736171444520e4576135a4f76835d
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-19 15:23:59 +0530

    Time: 0 ms (100.00%), Space: 43 MB (87.34%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,32 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] graph) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<graph.length;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<graph.length;i++){
-            for(int j=0;j<graph.length;j++){
-                if(graph[i][j]==1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
+        // List<List<Integer>> adj = new ArrayList<>();
+        // for(int i=0;i<graph.length;i++){
+        //     adj.add(new ArrayList<>());
+        // }
+        // for(int i=0;i<graph.length;i++){
+        //     for(int j=0;j<graph.length;j++){
+        //         if(graph[i][j]==1){
+        //             adj.get(i).add(j);
+        //             adj.get(j).add(i);
+        //         }
+        //     }
+        // }
         int count = 0;
         int [] vis = new int[graph.length];
         for(int i=0;i<graph.length;i++){
             if(vis[i]==0){
                 count++;
-                dfs(i,adj,vis);
+                dfs(i,graph,vis);
             }
         }
         return count;
     }
-    public void dfs(int node,List<List<Integer>> adj,int [] vis){
+    public void dfs(int node,int [][] graph,int [] vis){
         vis[node] = 1;
-        for(int nbr: adj.get(node)){
-            if(vis[nbr]==0){
-                dfs(nbr,adj,vis);
+        for(int j=0;j<graph.length;j++){
+            if(graph[node][j]==1 && vis[j]==0){
+                dfs(j,graph,vis);
             }
         }
     }
