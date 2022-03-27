7557840cfb80d59bdea5239e4ef3f565dafac4f0
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-27 13:40:54 +0530

    Time: 8 ms (83.28%), Space: 55.8 MB (12.63%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,34 +1,33 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        ArrayList<int[]> list = new ArrayList<>();
-        int [] st = new int[intervals.length];
-       int [] et = new int [intervals.length];
+    public int [][] merge(int [][] intervals){
+        ArrayList<int []> list = new ArrayList<>();
+        int [] st = new int [intervals.length];
+        int [] et = new int [intervals.length];
         
         for(int i=0;i<intervals.length;i++){
             st[i] = intervals[i][0];
-            et[i]  = intervals[i][1];
+            et[i] = intervals[i][1];
         }
         Arrays.sort(st);
         Arrays.sort(et);
+        
         int ptr = 0;
         int j = 0;
         
         while(ptr<intervals.length-1){
-            if(st[ptr+1]<=et[ptr]){
-                ptr++;
-            }
+            if(et[ptr]>=st[ptr+1]) ptr++;
             else{
-                int [] arr = new int[2];
-                arr[0]=st[j];
-                arr[1] = et[ptr];
-                list.add(arr);
-                j=ptr+1;
+                int [] tempans = new int[2];
+                tempans[0] = st[j];
+                tempans[1] = et[ptr];
+                list.add(tempans);
+                j = ptr+1;
                 ptr++;
             }
         }
-        int [] arr = new int[2];
+        int [] arr = new int [2];
         arr[0] = st[j];
-        arr[1]  = et[ptr];
+        arr[1] = et[ptr];
         list.add(arr);
         
         return list.toArray(new int[list.size()][2]);
