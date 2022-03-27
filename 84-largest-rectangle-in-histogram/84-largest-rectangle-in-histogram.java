cf8295c63df6ac440096f90fdf91da582baece82
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-27 19:51:25 +0530

    Time: 11 ms (94.75%), Space: 79.7 MB (41.29%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -19,41 +19,41 @@ class Solution {
         
         return max;
     }
-    public int[] nextSmallerRight(int[] arr){
-        Stack<Integer> st=new Stack<>();
-        int[] res=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            while(st.size()>0 && arr[st.peek()]>arr[i]){
-                res[st.pop()]=i;
-            }
-            st.push(i);
-        }
-        while(st.size()>0){
-            res[st.pop()]=arr.length;
-        }
-        return res;
-    }
-    public int[] nextSmallerLeft(int[] arr){
-        Stack<Integer> st=new Stack<>();
-        int[] res=new int[arr.length];
-        for(int i=arr.length-1;i>=0;i--){
-            while(st.size()>0 && arr[st.peek()]>arr[i]){
-                res[st.pop()]=i;
-            }
-            st.push(i);
-        }
-        while(st.size()>0){
-            res[st.pop()]=-1;
-        }
-        return res;
-    }
-    public int largestRectangleArea2(int[] heights) {
-        int[] nsr=nextSmallerRight(heights);
-        int[] nsl=nextSmallerLeft(heights);
-        int max=0;
-        for(int i=0;i<heights.length;i++){
-            max=Math.max(max,(nsr[i]-nsl[i]-1)*heights[i]);
-        }
-        return max;
-    }
+    // public int[] nextSmallerRight(int[] arr){
+    //     Stack<Integer> st=new Stack<>();
+    //     int[] res=new int[arr.length];
+    //     for(int i=0;i<arr.length;i++){
+    //         while(st.size()>0 && arr[st.peek()]>arr[i]){
+    //             res[st.pop()]=i;
+    //         }
+    //         st.push(i);
+    //     }
+    //     while(st.size()>0){
+    //         res[st.pop()]=arr.length;
+    //     }
+    //     return res;
+    // }
+    // public int[] nextSmallerLeft(int[] arr){
+    //     Stack<Integer> st=new Stack<>();
+    //     int[] res=new int[arr.length];
+    //     for(int i=arr.length-1;i>=0;i--){
+    //         while(st.size()>0 && arr[st.peek()]>arr[i]){
+    //             res[st.pop()]=i;
+    //         }
+    //         st.push(i);
+    //     }
+    //     while(st.size()>0){
+    //         res[st.pop()]=-1;
+    //     }
+    //     return res;
+    // }
+    // public int largestRectangleArea2(int[] heights) {
+    //     int[] nsr=nextSmallerRight(heights);
+    //     int[] nsl=nextSmallerLeft(heights);
+    //     int max=0;
+    //     for(int i=0;i<heights.length;i++){
+    //         max=Math.max(max,(nsr[i]-nsl[i]-1)*heights[i]);
+    //     }
+    //     return max;
+    // }
 }
