f1ba91d1f0d17f664837f43c16e1efb03a2973e1
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-28 10:19:18 +0530

    Added solution - LeetHub

---
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -0,0 +1,159 @@
+// { Driver Code Starts
+//Initial Template for JAVA
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+public class Tree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution ob = new Solution();
+
+                ArrayList<Integer> vec = ob.topView(root);
+                for(int x : vec)
+                    System.out.print(x + " ");
+                System.out.println();
+        	
+                t--;   
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Tuple{
+    Node node;
+    int level;
+    
+    Tuple(Node node,int level){
+        this.level = level;
+        this.node = node;
+    }
+}
+class Solution
+{
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        // add your code
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        Queue<Tuple> q = new LinkedList<>();
+        Map<Integer,Integer> map = new TreeMap<>();
+        q.offer(new Tuple(root,0));
+        while(!q.isEmpty()){
+            
+                Tuple front = q.poll();
+                if(!map.containsKey(front.level)) map.put(front.level,front.node.data);
+                
+                if(front.node.left!=null) q.offer(new Tuple(front.node.left,front.level-1));
+                if(front.node.right!=null) q.offer(new Tuple(front.node.right,front.level+1));
+            }
+            for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+                    list.add(entry.getValue());
+            }
+        return list;
+    }
+}
