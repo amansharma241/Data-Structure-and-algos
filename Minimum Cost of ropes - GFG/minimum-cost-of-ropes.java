e338ad023c879e837eba99b59cd2c949bd28397d
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-30 14:46:42 +0530

    Added solution - LeetHub

---
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.java
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+        
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			long arr[] = new long[n];
+			
+			for(int i=0; i<n; i++)
+				arr[i] = sc.nextLong();
+		    out.println(new Solution().minCost(arr, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to return the minimum cost of connecting the ropes.
+    long minCost(long nums[], int n) 
+    {
+        // your code here
+        long cost = 0;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for(long num : nums){
+            pq.add(num);
+        }
+        while(pq.size()>1){
+            long mini = pq.poll();
+            long smini = pq.poll();
+            
+            long sum = mini + smini;
+            cost+= sum;
+            pq.add(sum);
+        }
+        return cost;
+    }
+}
