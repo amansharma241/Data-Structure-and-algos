16cc67add945077238e47186abbc19b65f2682de
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-04-01 01:09:45 +0530

    Time: 9 ms (44.69%), Space: 59.5 MB (11.04%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -8,6 +8,7 @@ class Solution {
       for(int i = 1; i <= m; i++){
         for(int j = 1; j <= n; j++){
           if(matrix[i - 1][j - 1] == '1'){
+              // below is the main line...
             dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]),dp[i - 1][j - 1]) + 1;
             maxEdge = Math.max(maxEdge, dp[i][j]);
           }
