04f364f545c79b672f876b95493a3ce5b57078fd
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-31 21:01:05 +0530

    Time: 2 ms (100.00%), Space: 41.6 MB (94.07%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,26 +14,29 @@
  * }
  */
 class Solution {
-   // int count=0;
+    //int count;
     public int pathSum(TreeNode root, int target) {
         if(root==null) return 0;
         HashMap<Integer,Integer> map = new HashMap<>();
         map.put(0,1);
-       int ans = helper(root,target,0,map);
-        return ans;
+        //count = 0;
+        int currsum = 0;
+        return sumhelper(root,target,currsum,map);
     }
-    public int helper(TreeNode root,int target,int currsum,HashMap<Integer,Integer> map){
+    public int sumhelper(TreeNode root,int target,int currsum,HashMap<Integer,Integer> map){
         if(root==null) return 0;
-        int count=0;
+        int count = 0;
         currsum+=root.val;
         if(map.containsKey(currsum-target)){
             count+=map.get(currsum-target);
         }
-        map.put(currsum,map.getOrDefault(currsum,0)+1);
+        map.put(currsum,map.getOrDefault(currsum,0) + 1);
         
-        count+=helper(root.left,target,currsum,map);
-        count+=helper(root.right,target,currsum,map);
-        map.put(currsum,map.get(currsum)-1);
+        int left = sumhelper(root.left,target,currsum,map);
+        int right = sumhelper(root.right,target,currsum,map);
+        count = count + left + right;
+        map.put(currsum, map.get(currsum)-1);
+        
         return count;
     }
 }
