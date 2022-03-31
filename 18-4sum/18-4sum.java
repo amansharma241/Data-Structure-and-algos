c3b00616870bccf29d1f63ff18e660c29b0fa751
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-03-31 14:41:32 +0530

    Time: 17 ms (78.26%), Space: 44.5 MB (67.78%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,32 +1,28 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums==null || nums.length<4) return result;
         int n = nums.length;
-        if(n<4) return res;
-        
         Arrays.sort(nums);
         
         for(int i=0;i<n-3;i++){
-            if(i>0 && nums[i-1]==nums[i]) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int target_3 = target - nums[i];
             for(int j = i+1;j<n;j++){
-                //if(j>0 && nums[j-1]==nums[j]) continue;
                 int target_2 = target_3 - nums[j];
-                int lo  = j+1;
+                int lo = j+1;
                 int hi = n-1;
                 while(lo<hi){
                     int sum = nums[lo] + nums[hi];
-                    if(sum<target_2 ) lo++;
-                    else if(sum>target_2) hi--;
-                    
+                    if(sum>target_2) hi--;
+                    else if(sum<target_2) lo++;
                     else{
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[lo]);
-                        temp.add(nums[hi]);
-                        res.add(temp);
-                        
+                        ArrayList<Integer> subRes = new ArrayList<>();
+                        subRes.add(nums[i]);
+                        subRes.add(nums[j]);
+                        subRes.add(nums[lo]);
+                        subRes.add(nums[hi]);
+                        result.add(subRes);
                         while(lo<hi && nums[lo]==nums[lo+1]) lo++;
                         while(lo<hi && nums[hi]==nums[hi-1]) hi--;
                         lo++;
@@ -35,8 +31,7 @@ class Solution {
                 }
                 while(j+1<n && nums[j]==nums[j+1]) j++;
             }
-            //while(i+1<n && nums[i]==nums[i+1]) ++i;
         }
-        return res;
+        return result;
     }
 }
