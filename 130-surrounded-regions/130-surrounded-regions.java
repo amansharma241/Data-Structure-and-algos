c1815b972c6e09bf8303d4bc0f4c166f02e437a5
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-04-27 19:30:55 +0530

    Time: 2 ms (76.73%), Space: 51 MB (71.32%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -14,11 +14,11 @@ class Solution {
                 else if(board[i][j]=='*') board[i][j]='O';
             }
         }
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                System.out.print(board[i][j]);
-            }
-        }
+        // for(int i=0;i<board.length;i++){
+        //     for(int j=0;j<board[0].length;j++){
+        //         System.out.print(board[i][j]);
+        //     }
+        // }
     }
     public void dfs(int i,int j,char [][] board){
         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!='O') return;
