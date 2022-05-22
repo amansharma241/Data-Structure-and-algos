566be8fbddcc0dfb8f673a8117bb08bb196be33f
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-05-22 13:18:51 +0530

    Time: 3 ms (93.91%), Space: 40.4 MB (89.90%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,25 +1,19 @@
 class Solution {
     public int countSubstrings(String s) {
-        boolean [][] dp = new boolean[s.length()][s.length()];
-        int count=0;
-        for(int g=0;g<s.length();g++){
-            for(int i=0,j=g;j<s.length();i++,j++){
-                if(g==0){
-                    dp[i][j]=true;
-                }
-                else if(g==1){
-                    if(s.charAt(i)==s.charAt(j)) dp[i][j]=true;
-                    else dp[i][j]=false;
-                }
-                else{
-                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true) dp[i][j]=true;
-                    else dp[i][j]=false;
-                }
-                if(dp[i][j]) count++;
-            }
-            
-        }
+        int count = 0;
+        if (s == null || s.length() == 0) return 0;
         
-        return count; 
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            count+=extendPalindrome(s, i, i) + extendPalindrome(s, i, i + 1);        }
+        
+        return count;
+    }
+    
+    private int extendPalindrome(String s, int left, int right) {
+        int count=0;
+        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++; left--; right++;
+        }
+        return count;
     }
 }
