2ee6a22982ea02f6e3754ae3ab575b3392f83321
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-07-11 13:41:21 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (99.46%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,19 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-       int [] dp  = new int[n+1];
-        int ans1 = helper(n,dp);
-        return ans1;
-        
-    }
-    public int helper(int n,int[]dp){
         if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n]>0) return dp[n];
-        int num1 = helper(n-1,dp);
-        int num2 = helper(n-2,dp);
-        int ans = num1 + num2;
-        
-        return dp[n] = ans;
+        if(n==1) return 1;
+        int [] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i =2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
-    
 }
