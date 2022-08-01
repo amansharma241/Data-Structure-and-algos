5b2f650f1b485964ad719f9f7492d895d62f31c1
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-08-01 15:29:28 +0530

    Time: 16 ms (80.46%), Space: 42.2 MB (96.45%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,27 +1,19 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int [][] dp = new int[text1.length()+1][text2.length()+1];
-        int ans = helper(text1.length(),text2.length(),text1,text2,dp);
-        return ans;
+    public int longestCommonSubsequence(String s, String t) {
+        int[] r0 = new int[t.length() + 1];
+        int[] r1 = new int[t.length() + 1];
+        for (int i=s.length()-1; i>=0; i--) {
+            for (int j=t.length()-1; j>=0; j--) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    r0[j] = r1[j+1] + 1;
+                } else {
+                    r0[j] = Math.max(r1[j], r0[j+1]);
+                }
+            }
+            int[] tmp = r0;
+            r0 = r1;
+            r1 = tmp;
+        }
+        return r1[0];
     }
-    
-    // recursive solution
-     public static int helper(int i,int j, String s1 , String s2,int [][] dp){
-         if(i==0 || j==0){
-             return 0;
-         }
-         if(dp[i][j]!=0){
-             return dp[i][j];
-         }
-       else if(s1.charAt(i-1)==s2.charAt(j-1)){
-           dp[i][j] = 1 + helper(i-1,j-1,s1,s2,dp);  
-           return 1 + helper(i-1,j-1,s1,s2,dp);
-         }
-         else{
-            int left = helper(i-1,j,s1,s2,dp);
-             int right = helper(i,j-1,s1,s2,dp);
-             dp[i][j] = Math.max(left,right); 
-             return (Math.max(left,right));
-         }
-     }
 }
