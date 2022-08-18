494a1a7b814fd2f8714892239644ee7e52f8d0fa
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-08-18 20:22:34 +0530

    Time: 1 ms (96.29%), Space: 41.7 MB (80.68%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,42 +1,21 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-         // length up to the last travel + 1 day is good enough (no need for 365)
-    int lastDay = days[days.length - 1]; 
-    // dp[i] means up to i-th day the minimum cost of the tickets
-    int[] dp = new int[lastDay + 1]; 
-    boolean[] isTravelDays = new boolean[lastDay + 1];
-    // mark the travel days
-    for(int day : days) isTravelDays[day] = true;
-    
-    for(int i = 1; i <= lastDay; i++) {
-        if(!isTravelDays[i]) { // no need to buy ticket if it is not a travel day
-            dp[i] = dp[i - 1];
-            continue;
-        }
-        // select which type of ticket to buy
-        dp[i] = costs[0] + dp[i - 1]; // 1-day
-        dp[i] = Math.min(costs[1] + dp[Math.max(i - 7, 0)], dp[i]); // 7-day
-        dp[i] = Math.min(costs[2] + dp[Math.max(i - 30, 0)], dp[i]); // 30-day
+        if(days==null || costs==null) return 0;
+        int [] dp = new int[days.length+1];
+        return helper(0,days,costs,dp);
     }
-    return dp[lastDay];
-        // if(days==null || costs==null) return 0;
-        // int [] dp = new int[days.length];
-        // return helper(days,costs,0,dp);
-    }
-    public int helper(int [] days,int [] costs,int i,int [] dp){
-        if(i>=days.length) return 0;
-        if(dp[i]!=0) return dp[i];
-        int day1 = costs[0] + helper(days,costs,i+1,dp);
-        int k=i;
-        for(;k<days.length;k++){
-            if(days[k]>=days[i]+7) break;
-        }
-        int day7 = costs[1] + helper(days,costs,k,dp);
-        for(;k<days.length;k++){
-            if(days[k]>=days[i]+30) break;
-        }
-        int day30 = costs[2] + helper(days,costs,k,dp);
+    public int helper(int idx,int [] days,int [] costs,int [] dp){
+        if(idx>=days.length) return 0;
+        if(dp[idx]!=0) return dp[idx];
+        int op1 = costs[0] + helper(idx+1,days,costs,dp);
         
-      return dp[i] = Math.min(day30,(Math.min(day1,day7)));
+        int k;
+        for(k=idx;k<days.length && days[k]<days[idx]+7;k++) continue;
+        
+        int op2 = costs[1] + helper(k,days,costs,dp);
+        for(k=idx;k<days.length && days[k]<days[idx]+30;k++) continue;
+        
+        int op3 = costs[2] + helper(k,days,costs,dp);
+        return dp[idx] = Math.min(op1,Math.min(op2,op3));
     }
 }
