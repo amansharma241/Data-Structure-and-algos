d15c04914d44a028af3501ef768f09b84e9132aa
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-08-19 09:58:14 +0530

    Time: 24 ms (10.35%), Space: 42.6 MB (60.88%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -1,20 +1,21 @@
 class Solution {
-  int M = 1000000007;
-
-  public int findPaths(int m, int n, int N, int i, int j) {
-    int[][][] memo = new int[m][n][N + 1];
-    for (int[][] l : memo) for (int[] sl : l) Arrays.fill(sl, -1);
-    return findPaths(m, n, N, i, j, memo);
-  }
-
-  public int findPaths(int m, int n, int N, int i, int j, int[][][] memo) {
-    if (i == m || j == n || i < 0 || j < 0) return 1;
-    if (N == 0) return 0;
-    if (memo[i][j][N] >= 0) return memo[i][j][N];
-    memo[i][j][N] = (
-        (findPaths(m, n, N - 1, i - 1, j, memo) + findPaths(m, n, N - 1, i + 1, j, memo)) % M +
-        (findPaths(m, n, N - 1, i, j - 1, memo) + findPaths(m, n, N - 1, i, j + 1, memo)) % M
-    ) % M;
-    return memo[i][j][N];
-  }
+    int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int mod = (int)1e9+7;
+        int[][][] dp = new int[m][n][maxMove + 1];
+        for (int k = 1; k < maxMove + 1; ++k) {
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    for (int[] d: dirs) {
+                        int x = i + d[0], y = j + d[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n)
+                            dp[i][j][k] = (dp[i][j][k] + 1) % mod;
+                        else
+                            dp[i][j][k] = (dp[i][j][k] + dp[x][y][k - 1]) % mod;
+                    }
+                }
+            }
+        }
+        return dp[startRow][startColumn][maxMove];
+    }
 }
