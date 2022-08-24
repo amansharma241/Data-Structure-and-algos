7ed1063afc5aaa0890184de2f1f4e0d2c2326d24
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-08-24 19:08:41 +0530

    Time: 28 ms (55.32%), Space: 46.8 MB (67.38%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -0,0 +1,83 @@
+class Solution {
+    public String shortestCommonSupersequence(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0)
+                    dp[i][j] = j;
+                else if (j == 0)
+                    dp[i][j] = i;
+                else if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j],
+                            dp[i][j - 1]);
+            }
+        }
+        // int i = s1.length();
+        // int j = s2.length();
+        // StringBuilder sb = new StringBuilder();
+        // while(i>0 && j>0){
+        //     if(s1.charAt(i-1)==s2.charAt(j-1)){
+        //         sb.append(s1.charAt(i-1));
+        //         i--;
+        //         j--;
+        //     }
+        //     else if(dp[i-1][j]>dp[i][j-1]){
+        //         sb.append(s1.charAt(i-1));
+        //         i--;
+        //     }
+        //     else{
+        //         sb.append(s2.charAt(j-1));
+        //         j--;
+        //     }
+        // }
+        // if(i>0){
+        //     while(i>0){
+        //         sb.append(s1.charAt(i-1));
+        //         i--;
+        //     }
+        // }
+        // if(j>0){
+        //     while(j>0){
+        //         sb.append(s2.charAt(j-1));
+        //         j--;
+        //     }
+        // }
+        // return sb.reverse().toString();
+         int l = dp[m][n]; // Length of the ShortestSuperSequence
+        char[] arr = new char[l];
+        int i=m, j=n;
+        while(i>0 && j>0)
+        {
+            /* If current character in s1 and str2 are same, then
+             current character is part of shortest supersequence */
+            if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                arr[--l] = s1.charAt(i-1);
+                i--;j--;
+            }else if(dp[i-1][j]<dp[i][j-1]) {
+                arr[--l] = s1.charAt(i-1);
+                i--;
+            }
+            else {
+                arr[--l] = s2.charAt(j-1);
+                j--;
+            }
+        }
+        while (i > 0) {
+            arr[--l] = s1.charAt(i-1);
+            i--;
+        }
+        while (j > 0) {
+            arr[--l] = s2.charAt(j-1);
+            j--;
+        }
+        return new String(arr);
+    }
+}
