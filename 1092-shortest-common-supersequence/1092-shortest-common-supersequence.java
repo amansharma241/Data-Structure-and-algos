0ab414d5d8d5bd592c64760da12b570bb36d43a6
amansharma241 <84559875+amansharma241@users.noreply.github.com>
2022-08-24 19:28:31 +0530

    Time: 32 ms (41.63%), Space: 46.5 MB (75.55%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -9,10 +9,7 @@ class Solution {
         {
             for (int j = 0; j <= n; j++)
             {
-                if (i == 0)
-                    dp[i][j] = 0;
-                else if (j == 0)
-                    dp[i][j] = 0;
+               if(i==0 || j==0) dp[i][j]=0;
                 else if (s1.charAt(i - 1) == s2.charAt(j - 1))
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
